8508a5a9a73249cb890b9891ed10dfabad8a257e
syedanasraza <ar3942635@gmail.com>
2024-12-14 02:57:49 -0800

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("first number enter place sohw")
+let name = prompt("what is your name?" , "Guest")
+console.log(name)
+
+let deletepost = confirm("do you really wont to delete this post ")
+console.log(deletepost)
+
+if(deletepost){
+    console.log("tour post has been delete successfully ")
+}
+
+else{
+    console.log("your post has not been delete  ")
+}
